bd8b2e7fbc014368e8c0793b5decb1e751b8406d
Tsewang <tsewangjee@gmail.com>
2022-01-12 04:26:36 +0530

    initial commit

---
+++ WildLife.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace WildLife.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
